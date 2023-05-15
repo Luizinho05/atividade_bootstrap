4f9478136991f9daec96c0b131e1158a281f2ebf
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-05-15 14:47:58 -0300

    bootstrap

--- src/Inicio/index.js
+++ src/Inicio/index.js
@@ -9,10 +9,10 @@ export default function Inicio() {
                    <img src={MarioWorld} alt={MarioWorld} className='img-fluid' />
                 </div>
                 <div class="col-2">
-                   <h1>Coluna 2</h1>
+                   <h1>Coluna 2 Mario World vem do nintendo</h1>
                 </div>
                 <div class='col coluna3'>
-                   <h1>Coluna 3</h1>
+                   <h1>Coluna 3 Luighi é seu irmão e yoshi é pet de estimação da familia</h1>
                 </div>
             </div>
         </div>
